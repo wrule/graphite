a57762d739d538b708b5b59cbdced679cd329c03
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-26 15:36:58 +0800

    加载市场

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,20 @@ const secret = require('../.secret.json');
 
 async function main() {
   console.log('你好，世界');
-  const exchange = new ccxt.binance(secret.exchange);
-  const rsp = await exchange.fetchBalance();
-  console.log(rsp);
+  const exchange = new ccxt.binance({
+    ...secret.exchange,
+    options: {
+      defaultType: 'future',
+      hedgeMode: true,
+    },
+  });
+  // const rsp = await exchange.fetchBalance();
+  // console.log(rsp);
+  const markets = await exchange.loadMarkets();
+  Object.keys(markets)
+    .filter((key) => key.includes('ETH'))
+    .filter((key) => key.includes(':'))
+    .forEach((key) => console.log(key));
 }
 
 main();
