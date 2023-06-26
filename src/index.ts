9d0ff536b370e95df27f62144f793520ba26426c
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-26 22:31:28 +0800

    思考

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ const secret = require('../.secret.json');
 
 async function main() {
   console.log('你好，世界');
+  console.log(secret.exchange);
   const exchange = new ccxt.binance({
     ...secret.exchange,
-    // options: {
-    //   defaultType: 'future',
-    //   hedgeMode: true,
-    // },
+    options: {
+      defaultType: 'future',
+      hedgeMode: true,
+    },
   });
   // const rsp = await exchange.fetchBalance();
   // console.log(rsp);
@@ -21,7 +22,9 @@ async function main() {
     .filter((key) => key.includes(':'))
     .forEach((key) => console.log(key));
 
-  const rsp = await exchange.createMarketBuyOrder('ETH/USDT', 0.01);
+  const rsp = await exchange.createMarketSellOrder('ETH/USDT', 0.01, {
+    positionSide: 'LONG',
+  });
   fs.writeFileSync('output/output.json', JSON.stringify(rsp, null, 2));
   // const rsp = await exchange.fetchTrades('ETH/USDT:USDT', undefined, 3);
   // fs.writeFileSync('output-2.json', JSON.stringify(rsp, null, 2));
