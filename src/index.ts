816a0215f306b2badeee49d53438e6cc13cfc8f4
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-26 14:35:34 +0800

    一个简单的例子

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import ccxt from 'ccxt';
 
-console.log('你好，世界');
+const secret = require('../.secret.json');
+
+async function main() {
+  console.log('你好，世界');
+  const exchange = new ccxt.binance(secret.exchange);
+  const rsp = await exchange.fetchBalance();
+  console.log(rsp);
+}
+
+main();
