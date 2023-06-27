fe0223dc250319941d4450b10a8dfcc6fffba50e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-27 15:43:09 +0800

    提交代码

--- src/contract/binance.ts
+++ src/contract/binance.ts
@@ -10,8 +10,11 @@ class ContractBinance implements Contract {
     },
   ) { }
 
-  public async MarketLongBuy(funds: number) {
-
+  public async MarketLongBuy(assets: number) {
+    const amount = this.config.exchange.amountToPrecision(this.config.symbol, assets);
+    return this.config.exchange.createMarketBuyOrder(this.config.symbol, amount, {
+      positionSide: 'LONG',
+    });
   }
 
   public async MarketLongSell(assets: number) {
