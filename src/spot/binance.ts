1735b57bc911f6ff1877c10ce13c2f90eacfd324
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-27 15:18:07 +0800

    提交代码

--- src/spot/binance.ts
+++ src/spot/binance.ts
@@ -4,17 +4,19 @@ import { Spot } from '.';
 export
 class SpotBinance implements Spot {
   public constructor(
-    private readonly symbol: string,
-    private readonly exchange: binance,
+    private readonly config: {
+      symbol: string,
+      exchange: binance,
+    },
   ) { }
 
   public async MarketBuy(funds: number) {
-    const amount = this.exchange.costToPrecision(this.symbol, funds);
-    return this.exchange.createMarketBuyOrder(this.symbol, amount, { quoteOrderQty: amount });
+    const amount = this.config.exchange.costToPrecision(this.config.symbol, funds);
+    return this.config.exchange.createMarketBuyOrder(this.config.symbol, amount, { quoteOrderQty: amount });
   }
 
   public async MarketSell(assets: number) {
-    const amount = this.exchange.amountToPrecision(this.symbol, assets);
-    return this.exchange.createMarketSellOrder(this.symbol, amount);
+    const amount = this.config.exchange.amountToPrecision(this.config.symbol, assets);
+    return this.config.exchange.createMarketSellOrder(this.config.symbol, amount);
   }
 }
