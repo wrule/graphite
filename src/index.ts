82f0203809f040d7cf03efafa293c22ee3dfdd57
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-27 15:50:57 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,11 @@ import fs from 'fs';
 import ccxt from 'ccxt';
 import { Wallet } from './wallet';
 import { SpotBinance } from './spot/binance';
+import { ContractBinance } from './contract/binance';
 
 const secret = require('../.secret.json');
 
 async function main() {
-
-  const wallet = new Wallet({ 'USDT': 100 });
-  wallet.In('USDT', 100);
-  wallet.Out('USDT', 30);
-  wallet.Out('ETH', 1);
-  console.log(wallet.Get('USDT'));
-  console.log(wallet.States());
-
-  // return;
-  console.log('你好，世界');
-  console.log(secret.exchange);
   const exchange = new ccxt.binance({
     ...secret.exchange,
     // options: {
@@ -25,19 +15,15 @@ async function main() {
     //   hedgeMode: true,
     // },
   });
-  // const rsp = await exchange.fetchBalance();
-  // console.log(rsp);
   const markets = await exchange.loadMarkets();
   Object.keys(markets)
     .filter((key) => key.includes('ETH'))
     .filter((key) => key.includes(':'))
     .forEach((key) => console.log(key));
 
-  const spot = new SpotBinance({ symbol: 'ETH/USDT', exchange });
-  // const order = await spot.MarketSell(0.0058);
+  const contract = new ContractBinance({ symbol: 'ETH/USDT:USDT', exchange });
+  const order = await contract.MarketLongBuy(0.01);
   fs.writeFileSync('output/output.json', JSON.stringify(order, null, 2));
-  // const rsp = await exchange.fetchTrades('ETH/USDT:USDT', undefined, 3);
-  // fs.writeFileSync('output-2.json', JSON.stringify(rsp, null, 2));
 }
 
 main();
