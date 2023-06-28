5d46ee4d748f0919417383ee1891fc1669c6a2e0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 11:46:11 +0800

    提交代码

--- src/exchange/binance/index.ts
+++ src/exchange/binance/index.ts
@@ -5,3 +5,7 @@ interface Binance {
   MarketLongOpen(funds: number): Promise<Order>;
   MarketLongClose(assets: number): Promise<Order>;
 }
+
+export { BinanceSpot } from './spot';
+export { BinanceMargin } from './margin';
+export { BinanceFutures } from './futures';
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import 'global-agent/bootstrap';
 import fs from 'fs';
 import ccxt from 'ccxt';
-import { Wallet } from './wallet';
-import { SpotBinance } from './spot/binance';
-import { ContractBinance } from './contract/binance';
-import { MarginBinance } from './margin/binance';
+import { Binance, BinanceSpot } from './exchange/binance';
 
 const secret = require('../.secret.json');
 
 async function main() {
   const exchange = new ccxt.binance({
     ...secret.exchange,
-    options: {
-      defaultType: 'margin',
-      // hedgeMode: true,
-    },
+    // options: {
+    //   defaultType: 'margin',
+    //   // hedgeMode: true,
+    // },
   });
   const markets = await exchange.loadMarkets();
   Object.keys(markets)
@@ -22,8 +19,8 @@ async function main() {
     .filter((key) => key.includes(':'))
     .forEach((key) => console.log(key));
 
-  const ex = new MarginBinance({ symbol: 'ETH/USDT', exchange });
-  const order = await ex.MarketLongClose(0.0106);
+  const ex: Binance = new BinanceSpot({ symbol: 'ETH/USDT', exchange });
+  const order = await ex.MarketLongOpen(20);
   fs.writeFileSync('output/output.json', JSON.stringify(order, null, 2));
 }
 
