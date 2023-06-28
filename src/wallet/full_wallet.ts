08513e1acaed81eadae0056afc354a051989599f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 22:00:54 +0800

    提交代码

--- src/wallet/full_wallet.ts
+++ src/wallet/full_wallet.ts
@@ -13,24 +13,24 @@ extends Wallet {
   }
 
   public async OpenFull(symbol: string) {
-    // console.log(this.States());
+    console.log(this.States());
     const market = this.exchange.Exchange.market(symbol);
     const order = await this.exchange.MarketLongOpen(symbol, this.Get(market.quote));
     this.Send(market.quote, order.cost);
     this.Receive(market.base, order.amount);
     order.trades.forEach((trade) => this.Send(trade.fee.currency, trade.fee.cost));
-    // fs.writeFileSync('output/output-buy.json', JSON.stringify(order, null, 2));
-    // console.log(this.States());
+    fs.writeFileSync('output/output-buy.json', JSON.stringify(order, null, 2));
+    console.log(this.States());
   }
 
   public async CloseFull(symbol: string) {
-    // console.log(this.States());
+    console.log(this.States());
     const market = this.exchange.Exchange.market(symbol);
     const order = await this.exchange.MarketLongClose(symbol, this.Get(market.base));
     this.Send(market.base, order.amount);
     this.Receive(market.quote, order.cost);
     order.trades.forEach((trade) => this.Send(trade.fee.currency, trade.fee.cost));
-    // fs.writeFileSync('output/output-sell.json', JSON.stringify(order, null, 2));
-    // console.log(this.States());
+    fs.writeFileSync('output/output-sell.json', JSON.stringify(order, null, 2));
+    console.log(this.States());
   }
 }
