d2cc03fc139ab6ccf7a8d38485721ffb701ad485
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 14:47:26 +0800

    合约

--- src/exchange/binance/futures.ts
+++ src/exchange/binance/futures.ts
@@ -3,59 +3,39 @@ import { Binance } from '.';
 
 export
 class BinanceFutures implements Binance {
-  public constructor(private readonly symbol: string, config: any) {
-    this.exchange = new binance({
-      ...config,
-      options: {
-        defaultType: 'future',
-        hedgeMode: true,
-        ...config.options,
-      },
-    });
-  }
+  public constructor(public readonly Exchange: binance) { }
 
-  private exchange: binance;
-
-  public get Exchange() {
-    return this.exchange;
-  }
-
-  public async Init() {
-    await this.exchange.loadMarkets();
-    return this;
-  }
-
-  private async getLastAskPrice() {
-    const book = await this.exchange.fetchOrderBook(this.symbol, 10);
+  private async getLastAskPrice(symbol: string) {
+    const book = await this.Exchange.fetchOrderBook(symbol, 10);
     return book.asks[0][0];
   }
 
-  public async MarketLongOpen(funds: number) {
-    const price = await this.getLastAskPrice();
-    const amount = this.exchange.amountToPrecision(this.symbol, funds / price);
-    return this.exchange.createMarketBuyOrder(this.symbol, amount, {
+  public async MarketLongOpen(symbol: string, funds: number) {
+    const price = await this.getLastAskPrice(symbol);
+    const amount = this.Exchange.amountToPrecision(symbol, funds / price);
+    return this.Exchange.createMarketBuyOrder(symbol, amount, {
       positionSide: 'LONG',
     });
   }
 
-  public async MarketLongClose(assets: number) {
-    const amount = this.exchange.amountToPrecision(this.symbol, assets);
-    return this.exchange.createMarketSellOrder(this.symbol, amount, {
+  public async MarketLongClose(symbol: string, assets: number) {
+    const amount = this.Exchange.amountToPrecision(symbol, assets);
+    return this.Exchange.createMarketSellOrder(symbol, amount, {
       positionSide: 'LONG',
     });
   }
 
-  public async MarketShortOpen(funds: number) {
-    const price = await this.getLastAskPrice();
-    const amount = this.exchange.amountToPrecision(this.symbol, funds / price);
-    return this.exchange.createMarketSellOrder(this.symbol, amount, {
+  public async MarketShortOpen(symbol: string, funds: number) {
+    const price = await this.getLastAskPrice(symbol);
+    const amount = this.Exchange.amountToPrecision(symbol, funds / price);
+    return this.Exchange.createMarketSellOrder(symbol, amount, {
       positionSide: 'SHORT',
     });
   }
 
-  public async MarketShortClose(assets: number) {
-    const amount = this.exchange.amountToPrecision(this.symbol, assets);
-    return this.exchange.createMarketBuyOrder(this.symbol, amount, {
+  public async MarketShortClose(symbol: string, assets: number) {
+    const amount = this.Exchange.amountToPrecision(symbol, assets);
+    return this.Exchange.createMarketBuyOrder(symbol, amount, {
       positionSide: 'SHORT',
     });
   }
