346151e5a92497e0f892c3bb5a996d7914ddd050
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 11:21:04 +0800

    spot

--- src/spot/binance.ts
+++ src/spot/binance.ts
@@ -1,22 +1,25 @@
 import { binance } from 'ccxt';
-import { Spot } from '.';
+import { Margin } from '.';
 
 export
-class SpotBinance implements Spot {
-  public constructor(
-    private readonly config: {
-      symbol: string,
-      exchange: binance,
-    },
-  ) { }
+class MarginBinance implements Margin {
+  public constructor(private readonly config: {
+    symbol: string,
+    exchange: binance,
+  }) { }
 
-  public async MarketLongOpen(funds: number) {
+  public MarketLongOpen(funds: number) {
     const amount = this.config.exchange.costToPrecision(this.config.symbol, funds);
-    return this.config.exchange.createMarketBuyOrder(this.config.symbol, amount, { quoteOrderQty: amount });
+    return this.config.exchange.createMarketBuyOrder(this.config.symbol, amount, {
+      quoteOrderQty: amount,
+      type: 'margin',
+    });
   }
 
-  public async MarketLongClose(assets: number) {
+  public MarketLongClose(assets: number) {
     const amount = this.config.exchange.amountToPrecision(this.config.symbol, assets);
-    return this.config.exchange.createMarketSellOrder(this.config.symbol, amount);
+    return this.config.exchange.createMarketSellOrder(this.config.symbol, amount, {
+      type: 'margin',
+    });
   }
 }
