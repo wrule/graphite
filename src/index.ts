4a6f7298882ee22b652cd96e1498f4d7d0e094dc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 16:49:49 +0800

    合约测试通过

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import { FullWallet } from './wallet/full_wallet';
 const secret = require('../.secret.json');
 
 async function main() {
-  const exchange = await CreateBinanceMargin(secret.exchange);
-  const wallet = new FullWallet(exchange, { 'USDT': 0.09984007, 'ETH': 0.0160733 });
+  const exchange = await CreateBinanceFutures(secret.exchange);
+  const wallet = new FullWallet(exchange, { USDT: 1.4612000000000016, ETH: 0.01 });
   await wallet.SellFull('ETH/USDT');
   // const order = await ex.MarketShortClose('ETH/USDT', 0.01);
   // fs.writeFileSync('output/output.json', JSON.stringify(order, null, 2));
