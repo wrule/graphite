6c1eda5c7953a55bfa88f7c16412ae5bc3fd9bf3
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 22:11:30 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,10 @@ import { FullWallet } from './wallet/full_wallet';
 const secret = require('../.secret.json');
 
 async function main() {
-  const exchange = await CreateBinanceFutures(secret.exchange);
-  const wallet = new FullWallet(exchange, { USDT: 1.4612000000000016, ETH: 0.01 });
-  await wallet.SellFull('ETH/USDT');
+  const exchange = await CreateBinanceMargin(secret.exchange);
+  const wallet = new FullWallet(exchange, { USDT: 20 });
+  await wallet.OpenFull('ETH/USDT');
+  await wallet.CloseFull('ETH/USDT');
   // const order = await ex.MarketShortClose('ETH/USDT', 0.01);
   // fs.writeFileSync('output/output.json', JSON.stringify(order, null, 2));
 }
