e640c38b1d8d44b0baddd82702260db88a0506cb
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-28 15:13:49 +0800

    合约测试

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import 'global-agent/bootstrap';
 import fs from 'fs';
 import ccxt from 'ccxt';
-import { Binance, BinanceFutures, BinanceMargin, BinanceSpot } from './exchange/binance';
+import { CreateBinanceFutures } from './exchange/binance';
 
 const secret = require('../.secret.json');
 
 async function main() {
-  const ex: Binance = await new BinanceFutures('ETH/USDT', secret.exchange).Init();
-  const order = await ex.MarketLongClose(0.010);
+  const ex = await CreateBinanceFutures(secret.exchange);
+  const order = await ex.MarketShortClose('ETH/USDT', 0.01);
   fs.writeFileSync('output/output.json', JSON.stringify(order, null, 2));
 }
 
