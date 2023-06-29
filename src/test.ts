19e98207cd14f2a67d03e4c2c4fea58e2a5a2c12
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-29 14:53:07 +0800

    提交代码

--- src/test.ts
+++ src/test.ts
@@ -8,9 +8,7 @@ async function main() {
   const spot = await CreateOKXSpot(secret.exchange);
   const b = await spot.Exchange.fetchBalance();
   console.log(b);
-  let order = await spot.MarketLongClose('ETH/USDT', 0.01086913);
-  order = await spot.Exchange.fetchOrder(order.id, 'ETH/USDT');
-  fs.writeFileSync('output/output-okx-sell-detail.json', JSON.stringify(order, null, 2));
+  await spot.MarketLongClose('ETH/USDT', 0.010862127);
 }
 
 main();
