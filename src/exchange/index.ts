6494be19caf90aa256a9b7847d4e56fb691eff9a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-29 22:53:29 +0800

    提交代码

--- src/exchange/index.ts
+++ src/exchange/index.ts
@@ -1,8 +1,15 @@
-import { Order } from 'ccxt';
+import { Exchange, Order } from 'ccxt';
 
 export
 interface OrderX extends Order {
   start_time: number;
   end_time: number;
   fee_list: { currency: string, cost: number }[];
+}
+
+export
+interface ExchangeX {
+  Exchange: Exchange,
+  MarketLongOpen(symbol: string, funds: number): Promise<OrderX>;
+  MarketLongClose(symbol: string, assets: number): Promise<OrderX>;
 }
