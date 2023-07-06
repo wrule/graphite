52bc37b374f0ed1ff3dacec20f65a579b15b716d
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-06 13:54:18 +0800

    提交代码

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,15 @@
 import 'global-agent/bootstrap';
 import { FullTrader } from './trader/full_trader';
 import { CreateBinanceFuturesLong } from './exchange/binance/futures_long';
+import { CreateBinanceFuturesShort } from './exchange/binance/futures_short';
+import { CreateBinanceSpot } from './exchange/binance/spot';
 import fs from 'fs';
 
 const secret = require('../.secret.json');
 
 async function main() {
-  const spot = await CreateBinanceFuturesLong(secret.exchange);
-  const trader = new FullTrader(spot, { USDT: 20 });
+  const spot = await CreateBinanceSpot(secret.exchange);
+  const trader = new FullTrader(spot, { USDT: 12 });
   const open_order = await trader.MarketOpenFull('ETH/USDT');
   fs.writeFileSync('output/fopen.json', JSON.stringify(open_order, null, 2));
   const close_order = await trader.MarketCloseFull('ETH/USDT');
