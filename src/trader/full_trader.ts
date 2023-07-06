4bfc72b9ef4bf541d3cc1a540e500cbc46800874
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-06 13:40:01 +0800

    删除注释

--- src/trader/full_trader.ts
+++ src/trader/full_trader.ts
@@ -11,24 +11,20 @@ class FullTrader extends Trader {
   }
 
   public async MarketOpenFull(symbol: string) {
-    // console.log(this.States());
     const market = this.exchange.Exchange.market(symbol);
     const order = await this.exchange.MarketOpen(symbol, this.Get(market.quote));
     this.Send(market.quote, order.cost);
     this.Receive(market.base, order.amount);
     order.fee_list.forEach((fee) => this.Send(fee.currency, fee.cost));
-    // console.log(this.States());
     return order;
   }
 
   public async MarketCloseFull(symbol: string) {
-    // console.log(this.States());
     const market = this.exchange.Exchange.market(symbol);
     const order = await this.exchange.MarketClose(symbol, this.Get(market.base));
     this.Send(market.base, order.amount);
     this.Receive(market.quote, order.cost);
     order.fee_list.forEach((fee) => this.Send(fee.currency, fee.cost));
-    // console.log(this.States());
     return order;
   }
 }
