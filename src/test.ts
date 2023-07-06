26ec1078de9c0705bda3a8822376fe419782bd46
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-06 13:43:57 +0800

    提交代码

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,4 @@
 import 'global-agent/bootstrap';
-import { CreateOKXSpotExchange } from './exchange/okx/spot';
 import { FullTrader } from './trader/full_trader';
 import { CreateBinanceFuturesLong } from './exchange/binance/futures_long';
 import fs from 'fs';
@@ -8,16 +7,11 @@ const secret = require('../.secret.json');
 
 async function main() {
   const spot = await CreateBinanceFuturesLong(secret.exchange);
-  
-  // const a = await spot.Exchange.market('ETH/USDT');
-  // fs.writeFileSync('output/a.json', JSON.stringify(a, null, 2));
-
-  // const spot = await CreateOKXSpotExchange(secret.exchange);
   const trader = new FullTrader(spot, { USDT: 20 });
   const open_order = await trader.MarketOpenFull('ETH/USDT');
-  fs.writeFileSync('output/fbuy.json', JSON.stringify(open_order, null, 2));
+  fs.writeFileSync('output/fopen.json', JSON.stringify(open_order, null, 2));
   const close_order = await trader.MarketCloseFull('ETH/USDT');
-  fs.writeFileSync('output/fsell.json', JSON.stringify(close_order, null, 2));
+  fs.writeFileSync('output/fclose.json', JSON.stringify(close_order, null, 2));
 }
 
 main();
